545f5c32d13d7a42c886c38d01c83eac390351d0
Ilona Koda <ilona.koda@amberbit.com>
2018-11-20 11:32:18 +0100

    Add React to project

---
+++ apps/ui/assets/js/hello-react.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import HelloReact from "./components/HelloReact"
+
+ReactDOM.render(
+  <HelloReact/>,
+  document.getElementById("hello-react")
+)
