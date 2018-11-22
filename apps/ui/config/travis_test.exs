f0c2f93986d7f2045693c2475057c1c1f2336a9b
Hubert Łępicki <hubert.lepicki@amberbit.com>
2018-11-22 13:05:45 +0100

    add travis config

---
+++ apps/ui/config/travis_test.exs
@@ -0,0 +1,26 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :ui, UiWeb.Endpoint,
+  http: [port: 4002],
+  server: true
+
+config :ui, :sql_sandbox, true
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :ui, Ui.Repo,
+  username: "postgres",
+  password: "",
+  database: "ui_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure wallaby
+
+config :wallaby,
+  driver: Wallaby.Experimental.Chrome,
+  chrome: [headless: true]
