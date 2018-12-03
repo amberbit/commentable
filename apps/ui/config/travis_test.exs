3ae7a6ea327f26de30ab85176c6abe16a1023ccd
Hubert Łępicki <hubert.lepicki@amberbit.com>
2018-12-03 09:58:14 +0100

    attempt to fix travis build

--- apps/ui/config/travis_test.exs
+++ apps/ui/config/travis_test.exs
@@ -15,7 +15,6 @@ config :logger, level: :warn
 config :ui, Ui.Repo,
   adapter:  Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "",
   database: "ui_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
